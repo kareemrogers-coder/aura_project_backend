a5470e704261932af352ee5d04a650f2f25dec20
kareemrogers-coder <kareemrogers27@gmail.com>
2025-01-13 20:31:58 -0500

    commit 33

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ class TestingConfig:
 class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_SQLALCHEMY_DATABASE_URI')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    JWT_ALGORITHM = os.environ.get('JWT_ALGORITHM')
+    # JWT_ALGORITHM = os.environ.get('JWT_ALGORITHM')
     CACHE_TYPE = 'SimpleCache'
