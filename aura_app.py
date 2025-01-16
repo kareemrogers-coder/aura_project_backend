dea553ad92a5967803358723e3b0c65c58633202
kareemrogers-coder <kareemrogers27@gmail.com>
2025-01-16 03:48:14 -0500

    commit 39

--- aura_app.py
+++ aura_app.py
@@ -18,7 +18,7 @@ ALGOTITHMS = ["RS256"]
 
 #if __name__ == '__main__': # code was commented out to set enivornment for render
 with app.app_context(): # code was indented out one tab.
-                #db.drop_all()
-                db.create_all()
+                db.drop_all()
+                #db.create_all()
     
 #app.run(debug=True) # code was commented out to set up enivornment for render
